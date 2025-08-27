52e3a0c6d818c9ee410169cb8b6e45395bce3173
Tony Qiu <minxuan0429@gmail.com>
2025-08-27 10:39:07 +0800

    建立廣告的Model

---
+++ src/main/java/com/tibame/actad/model/ActAdVo.java
@@ -0,0 +1,19 @@
+package com.tibame.actad;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class ActAdVo {
+    private Integer actadId;
+    private byte[] actadImg;
+    private byte actadRevStat;
+    private Timestamp actadRevupd;
+    private String actadRevremark;
+    private Integer actadLaunStat; 
+    private Timestamp actadLaunupd;
+    private Date actadStart;
+    private Date actadEnd;
+    private Integer fMemId;
+    private Integer actId;
+    
+}
