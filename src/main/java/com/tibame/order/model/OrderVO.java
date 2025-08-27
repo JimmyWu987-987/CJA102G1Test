bf16ed3fcd3f17aa66dd6b7633eaf53bc70a6e1f
JimmyWu <workjimmywu0820@gmail.com>
2025-08-27 10:09:35 +0800

    first commit.

---
+++ src/main/java/com/tibame/order/model/OrderVO.java
@@ -0,0 +1,18 @@
+package com.tibame.order.model;
+
+public class OrderVO {
+	private Integer ordId;
+	private Integer menId;
+	public Integer getOrdId() {
+		return ordId;
+	}
+	public void setOrdId(Integer ordId) {
+		this.ordId = ordId;
+	}
+	public Integer getMenId() {
+		return menId;
+	}
+	public void setMenId(Integer menId) {
+		this.menId = menId;
+	}
+}
