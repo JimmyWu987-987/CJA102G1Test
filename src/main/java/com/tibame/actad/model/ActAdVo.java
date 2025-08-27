4d7ff07fc36bdfbad035b2da7b4a8ca231253cbe
Tony Qiu <minxuan0429@gmail.com>
2025-08-27 11:01:10 +0800

    1

--- src/main/java/com/tibame/actad/model/ActAdVo.java
+++ src/main/java/com/tibame/actad/model/ActAdVo.java
@@ -1,19 +1,20 @@
-package com.tibame.actad;
+package com.tibame.actad.model;
 
 import java.sql.Date;
 import java.sql.Timestamp;
 
 public class ActAdVo {
-    private Integer actadId;
-    private byte[] actadImg;
-    private byte actadRevStat;
-    private Timestamp actadRevupd;
-    private String actadRevremark;
-    private Integer actadLaunStat; 
-    private Timestamp actadLaunupd;
-    private Date actadStart;
-    private Date actadEnd;
+    private Integer actAdId;
+    private byte[] actAdImg;
+    private byte actAdRevStat;
+    private Timestamp actAdRevupd;
+    private String actAdRevremark;
+    private Integer actAdLaunStat; 
+    private Timestamp actAdLaunUpd;
+    private Date actAdStart;
+    private Date actAdEnd;
+    private Integer actAdFee;
+    private Date actadFeeEnd;
     private Integer fMemId;
     private Integer actId;
-    
 }
