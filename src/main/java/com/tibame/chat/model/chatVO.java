0daa9edac0f52d0ed0265e23d98163d5776e7399
ChihWei <jackyluings@gmail.com>
2025-08-27 11:24:51 +0800

    get set

--- src/main/java/com/tibame/chat/model/chatVO.java
+++ src/main/java/com/tibame/chat/model/chatVO.java
@@ -2,6 +2,14 @@ package com.tibame.chat.model;
 
 public class chatVO {
 	private Integer memID;
+
+	public Integer getMemID() {
+		return memID;
+	}
+
+	public void setMemID(Integer memID) {
+		this.memID = memID;
+	}
 	
 
 }
