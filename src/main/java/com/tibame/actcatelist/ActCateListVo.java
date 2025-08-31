b5e7f5aba095ba1fa3f4e45c26b06156a043c015
Jamie <yukige616@gmail.com>
2025-08-31 20:30:35 +0800

    update ActCateListVo

--- src/main/java/com/tibame/actcatelist/ActCateListVo.java
+++ src/main/java/com/tibame/actcatelist/ActCateListVo.java
@@ -3,5 +3,31 @@ package com.tibame.actcatelist;
 import java.io.Serializable;
 
 public class ActCateListVo implements Serializable {
+	private Integer actId;
+	private Integer actCateId;
+	
+	
+	public ActCateListVo() {
+		super();
+	}
+	
+	public ActCateListVo(Integer actId, Integer actCateId) {
+		this.actId = actId;
+		this.actCateId = actCateId;
+	}
 
+	
+	public Integer getActId() {
+		return actId;
+	}
+	public void setActId(Integer actId) {
+		this.actId = actId;
+	}
+	public Integer getActCateId() {
+		return actCateId;
+	}
+	public void setActCateId(Integer actCateId) {
+		this.actCateId = actCateId;
+	}	
+	
 }
